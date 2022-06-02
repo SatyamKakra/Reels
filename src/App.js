0bf2b00fb55ea511d82f2b7749f0b79576da34b2
satyam kakra <satyamkakra4u@gmail.com>
2022-06-03 00:14:20 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import Feed from './components/Feed';
+import Login from './components/Login';
+import PageNotFound from './components/PageNotFound';
+import Profile from './components/Profile';
+import Signup from './components/Signup';
+import { Switch, Route, Redirect } from "react-router-dom";
 
 
 function App() {
   return (
-    <div>Reels Project</div>
+    <>
+      <Switch>
+
+        {/* which component should render on which path */}
+
+        <Route path="/feed">
+        <Feed></Feed>
+        </Route>
+
+        <Route path="/login">             
+        <Login></Login>
+        </Route>
+
+        <Route path="/signup">
+        <Signup></Signup>
+        </Route>
+
+        <Route path="/profile">
+        <Profile></Profile>
+        </Route>
+
+        <Route>
+          <PageNotFound></PageNotFound>
+        </Route>
+
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <input type="Enter Email" />
+        <input type="Password" />
+
+        <button>Login</button>
+    </div>
+  )
+}
+
+export default Login
