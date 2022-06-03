e702b61a11a798928cc1df4117ba44ebd00a69fb
satyam kakra <satyamkakra4u@gmail.com>
2022-06-03 18:21:44 +0530

    login password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {auth} from "../firebase";
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 function Login() {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  const trackEmail = function (e) {
+    setEmail(e.target.value);
+  }
+
+  const trackPassword = function (e) {
+    setPassword(e.target.value);
+  }
+
+  const printDetails = async function (e) {
+    alert(email + " " + password);
+    
+    let userCred = await signInWithEmailAndPassword(auth, email, password)
+    console.log(userCred.user);
+  //     .then((userCredential) => {
+  //       // Signed in
+  //       var user = userCredential.user;
+  //       // ...
+  //     })
+  //     .catch((error) => {
+  //       var errorCode = error.code;
+  //       var errorMessage = error.message;
+  //     });
+
+  }
+
+
   return (
     <div>
-        <input type="Enter Email" />
-        <input type="Password" />
-
-        <button>Login</button>
+      <input type="email" onChange={trackEmail} value={email} placeholder="email"></input>
+      <br></br>
+      <input type="password" onChange={trackPassword} value={password} placeholder="password"></input>
+      <br></br>
+      <button type='click' onClick={printDetails}>Login</button>
     </div>
   )
 }
---
+++ src/secrets.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBGEM1FMNEqPRtpgqHW-jebKKDn_Q96FeA",
+    authDomain: "reels-auth-d0964.firebaseapp.com",
+    projectId: "reels-auth-d0964",
+    storageBucket: "reels-auth-d0964.appspot.com",
+    messagingSenderId: "274323201273",
+    appId: "1:274323201273:web:b02c01ae3b211390121628",
+    measurementId: "G-RDF83XVHGS"
+  };
+
+module.exports=firebaseConfig;
