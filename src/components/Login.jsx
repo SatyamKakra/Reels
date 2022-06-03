f7627f809d5d94be7c935b6230aefc8e26d55547
satyam kakra <satyamkakra4u@gmail.com>
2022-06-03 19:16:44 +0530

    login signout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import {auth} from "../firebase";
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 function Login() {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
+  let [user , setUser] = useState(null);
+  let [loader , setLoader] = useState(false);
+  let [error , setError] = useState("");
 
   const trackEmail = function (e) {
     setEmail(e.target.value);
@@ -15,10 +18,22 @@ function Login() {
   }
 
   const printDetails = async function (e) {
-    alert(email + " " + password);
+    // alert(email + " " + password);
+    try{
+
+      setLoader(true);
+      let userCred = await signInWithEmailAndPassword(auth, email, password)
+      // console.log(userCred.user);
+      setUser(userCred.user);
+    }catch (err){
+      setError(err.message);
+      // after some time -> error message remove
+      setTimeout(()=>{
+        setError("")
+      }, 2000)
+    }
+    setLoader(false);
     
-    let userCred = await signInWithEmailAndPassword(auth, email, password)
-    console.log(userCred.user);
   //     .then((userCredential) => {
   //       // Signed in
   //       var user = userCredential.user;
@@ -30,16 +45,36 @@ function Login() {
   //     });
 
   }
+  const signout = async function (){
+    await signOut(auth);
+    setUser(null);
+  }
 
 
   return (
-    <div>
-      <input type="email" onChange={trackEmail} value={email} placeholder="email"></input>
-      <br></br>
-      <input type="password" onChange={trackPassword} value={password} placeholder="password"></input>
-      <br></br>
-      <button type='click' onClick={printDetails}>Login</button>
-    </div>
+    <>
+
+    {
+
+      error != "" ? <h1>Error is {error}</h1>:
+     loader == true ? <h1>...Loading</h1>:
+    user != null ? 
+    
+      <>
+      <button onClick={signout}>Signout</button>
+      <h1>user is {user.uid}</h1>
+      </>
+      :
+    <>
+    <input type="email" onChange={trackEmail} value={email} placeholder="email"></input>
+    <br></br>
+    <input type="password" onChange={trackPassword} value={password} placeholder="password"></input>
+    <br></br>
+    <button type='click' onClick={printDetails}>Login</button>
+    </>
+
+    }    
+    </>
   )
 }
 
