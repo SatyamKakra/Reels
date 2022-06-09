67eb0f6b295a83a438aef9c6020048f8d303d9a3
satyam kakra <satyamkakra4u@gmail.com>
2022-06-10 00:10:18 +0530

    context

--- src/App.js
+++ src/App.js
@@ -5,36 +5,40 @@ import PageNotFound from './components/PageNotFound';
 import Profile from './components/Profile';
 import Signup from './components/Signup';
 import { Switch, Route, Redirect } from "react-router-dom";
+import { AuthContextProvider } from './context/AuthContext';
 
 
 function App() {
   return (
     <>
-      <Switch>
 
-        {/* which component should render on which path */}
+      {/* which component should render on which path */}
+      <AuthContextProvider>
+        <Switch>
 
-        <Route path="/feed">
-        <Feed></Feed>
-        </Route>
+          <Route path="/feed">
+            <Feed></Feed>
+          </Route>
 
-        <Route path="/login">             
-        <Login></Login>
-        </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
 
-        <Route path="/signup">
-        <Signup></Signup>
-        </Route>
+          <Route path="/signup">
+            <Signup></Signup>
+          </Route>
 
-        <Route path="/profile">
-        <Profile></Profile>
-        </Route>
+          <Route path="/profile">
+            <Profile></Profile>
+          </Route>
 
-        <Route>
-          <PageNotFound></PageNotFound>
-        </Route>
+          <Route>
+            <PageNotFound></PageNotFound>
+          </Route>
 
-      </Switch>
+        </Switch>
+      </AuthContextProvider>
+
     </>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./profile.css"
+import { useContext } from 'react'
+import {AuthContext} from "../context/AuthContext";
 
 function Profile() {
+  let contextObj = useContext(AuthContext);
+  let [loading, setLoading] = useState("");
+  
   return (
     <>
-    <div className="header"></div>
-    <div className="main">
+    {contextObj.cUser==null?<div>Need to Login</div>:
+    <div>Logged in user {contextObj.cUser.uid}</div>
+    // loading == true ? <div>getting data</div>:
 
-    <div className="pimg_container">
-
-    <img src="https://loremflickr.com/640/360
-" alt="" className="pimg" />
-    </div>
-    <div className="details">
-    <div className="content">Satyam</div>
-    <div className="content">No Of Posts: <span className="bold_text">Posts</span></div>
-    <div className="content">Email <span className="bold_text">Email.com</span></div>
-    </div>
-</div>
-    </>
-  )
+    // <>
+    // <div className="header"></div>
+    // <div className="main">
+    
+    // <div className="pimg_container">
+    
+    // <img src="https://loremflickr.com/640/360
+    // " alt="" className="pimg" />
+    // </div>
+    // <div className="details">
+    // <div className="content">Satyam</div>
+    // <div className="content">No Of Posts: <span className="bold_text">Posts</span></div>
+    // <div className="content">Email <span className="bold_text">Email.com</span></div>
+    // </div>
+    // </div>
+    // </>    
+}
+</>
+   
+    )
 }
 
 export default Profile
