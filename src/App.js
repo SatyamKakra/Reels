98313cef7f54afe0253072bc04d963663498d3db
satyam kakra <satyamkakra4u@gmail.com>
2022-06-11 14:54:28 +0530

    upto reels

--- src/App.js
+++ src/App.js
@@ -1,90 +1,81 @@
-import React, {Component, useContext} from 'react';
-import Feed from './components/Feed';
-import Login from './components/Login';
-import PageNotFound from './components/PageNotFound';
-import Profile from './components/Profile';
-import Signup from './components/Signup';
+// npm i react-router-dom@5.3.1
+import React, { useContext } from 'react';
+import Feed from "./components/Feed";
+import Login from "./components/Login";
+import PageNotFound from "./components/PageNotFound";
+import Profile from "./components/Profile";
+import Signup from "./components/Signup";
+import { AuthContextProvider, AuthContext } from "./context/AuthContext";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { AuthContextProvider, AuthContext } from './context/AuthContext';
-
-
 function App() {
   return (
     <>
-
       {/* which component should render on which path */}
       <AuthContextProvider>
         <Switch>
-
-
           <RedirectToFeed path="/login" comp={Login}>
-        
           </RedirectToFeed>
-
           <RedirectToFeed path="/signup" comp={Signup}>
-            
           </RedirectToFeed>
-
-          <PrivateRoute path="/feed" comp={Feed}>
-            
+          <PrivateRoute path="/feed"
+            comp={Feed}
+          >
           </PrivateRoute>
-
-          <PrivateRoute path="/profile" comp={Profile}>
-            
+          <PrivateRoute path="/profile"
+            comp={Profile}
+          >
+          </PrivateRoute>
           <Redirect from="/" to="/feed"></Redirect>
-
-          </PrivateRoute>
-
           <Route>
             <PageNotFound></PageNotFound>
           </Route>
-
         </Switch>
       </AuthContextProvider>
-
     </>
   );
 }
-function PrivateRoute(props){
+function PrivateRoute(props) {
   console.log("private Route", props)
-  let component = props.comp;
-  // cheack -> are you logged in
+  let Component = props.comp;
+  // check -> are you loggedIN
   let cUser = useContext(AuthContext);
-  //cUser -> null -> login page
-  // cUser -> anything
-  return(
-    <Route {...props}
-    render={
-      (props)=>{
-      //logic
-      return cUser != null ? <component{...props}></component>  :
-      <Redirect {...props} to="/login"></Redirect>
+  // cuser-> null ->logi page
+  // cuser-> anything 
+  return (
+    <Route
+      {...props}
+      render={
+        (props) => {
+          // logic
+          return cUser != null ? <Component {...props}>
+          </Component> : <Redirect
+            {...props}
+            to="/login"></Redirect>
+        }
       }
-    }>
-
-    </Route>
-  )
-}
-function RedirectToFeed(props){
-  let component = props.comp;
-  // cheack -> are you logged in
-  let cUser = useContext(AuthContext);
-  //cUser -> null -> login page
-  // cUser -> anything
-  return(
-    <Route {...props}
-    render={
-      (props)=>{
-      //logic
-      return cUser != null ? <Redirect{...props} to="/feed"></Redirect>:
-      <Component {...props}>
-
-      </Component>
-      }
-    }>
-
-    </Route>
+    ></Route>
   )
 }
 
+function RedirectToFeed(props) {
+  let Component = props.comp;
+  // check -> are you loggedIN
+  let cUser = useContext(AuthContext);
+  // cuser-> null ->logi page
+  // cuser-> anything 
+  return (
+    <Route
+      {...props}
+      render={
+        (props) => {
+          // logic
+          return cUser != null ? <Redirect {...props} to="/feed">
+          </Redirect> : <Component
+            {...props}
+          ></Component>
+        }
+      }
+    ></Route>
+  )
+}
 export default App;
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,7 +8,7 @@ function Feed() {
       <img src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png" alt="" className='insta_img' />
       <img src="https://media-exp2.licdn.com/dms/image/C4E03AQGwZ5l_z5sA2Q/profile-displayphoto-shrink_200_200/0/1646319637527?e=1660176000&v=beta&t=ATbHFGoXT9vE7IYLFeRkhOjS5PqI_h0nmE56YFN95T8" alt=""  className='Profile_img'/>
     </div>
-    <div className="main_continer"></div>
+    <div className="main_continer">
     <div className="upload_container">
     <i 
             className="movie_icon fa-solid fa-clapperboard"
@@ -16,6 +16,7 @@ function Feed() {
           <div className="upload_text">UPLOAD</div>
       </div>
     <div className="reels_container">Reels</div>
+    </div>
     </>
   )
 }
