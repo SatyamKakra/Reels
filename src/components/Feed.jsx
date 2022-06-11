b565751fc61d42d3ba4bc63f6924041c6fae0c2c
satyam kakra <satyamkakra4u@gmail.com>
2022-06-11 19:04:43 +0530

    reels

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import "./feed.css"
+import React from 'react';
+import "./feed.css";
 
 function Feed() {
   return (
     <>
-    <div className="header">
-      <img src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png" alt="" className='insta_img' />
-      <img src="https://media-exp2.licdn.com/dms/image/C4E03AQGwZ5l_z5sA2Q/profile-displayphoto-shrink_200_200/0/1646319637527?e=1660176000&v=beta&t=ATbHFGoXT9vE7IYLFeRkhOjS5PqI_h0nmE56YFN95T8" alt=""  className='Profile_img'/>
+      <div className="header">
+        <img src="
+        https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png
+        " alt="" className="insta_img" />
+     
+      <img src="https://media-exp2.licdn.com/dms/image/C4E03AQGwZ5l_z5sA2Q/profile-displayphoto-shrink_200_200/0/1646319637527?e=1660176000&v=beta&t=ATbHFGoXT9vE7IYLFeRkhOjS5PqI_h0nmE56YFN95T8" alt=""  className='profile_img'/>
     </div>
     <div className="main_continer">
-    <div className="upload_container">
-    <i 
+        <div className="upload_container">
+          <i 
             className="movie_icon fa-solid fa-clapperboard"
           ></i>
           <div className="upload_text">UPLOAD</div>
+        </div>
+        <div className="reels_container">Reels</div>
       </div>
-    <div className="reels_container">Reels</div>
-    </div>
     </>
   )
 }
-
 export default Feed;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,54 +1,49 @@
 import React, { useState, useEffect } from 'react'
-import { auth } from "../firebase";
-import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
-
+import { auth, } from "../firebase";
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 function Login() {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
+
   let [user, setUser] = useState(null);
   let [loader, setLoader] = useState(false);
   let [error, setError] = useState("");
- 
 
   const trackEmail = function (e) {
     setEmail(e.target.value);
   }
-
   const trackPassword = function (e) {
     setPassword(e.target.value);
   }
-
   const printDetails = async function () {
     // alert(email + " " + password);
     try {
-
       setLoader(true);
-      let userCred = await signInWithEmailAndPassword(auth, email, password)
+      // signin call hoga 
+      let userCred = await
+        signInWithEmailAndPassword(auth, email, password)
       // console.log(userCred.user);
       setUser(userCred.user);
     } catch (err) {
       setError(err.message);
-      // after some time -> error message remove
+      // after some time -> error message remove 
       setTimeout(() => {
         setError("")
       }, 2000)
     }
     setLoader(false);
-
   }
   const signout = async function () {
+    // ye call hoga 
     await signOut(auth);
     setUser(null);
   }
-
-
   useEffect(() => {
-    // aake call ho gya hai
     onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
-        setUser(user);
+        setUser(user)
         // ...
       } else {
         // User is signed out
@@ -58,34 +53,28 @@ function Login() {
       
     });
   }, []);
-
-  
-
   return (
     <>
-
       {
-        // maiLoader==true?<h1>Page Loading...</h1>:
-        error != "" ? <h1>Error is {error}</h1> :
+       error != "" ? <h1>Error is {error}</h1> :
           loader == true ? <h1>...Loading</h1> :
             user != null ?
-
               <>
-                <button onClick={signout}>Signout</button>
+                <button
+                  onClick={signout}
+                >Signout</button>
                 <h1>user is {user.uid}</h1>
               </>
               :
-              <>
-                <input type="email" onChange={trackEmail} value={email} placeholder="email"></input>
+              <><input type="email" onChange={trackEmail} value={email} placeholder="email" ></input>
                 <br></br>
-                <input type="password" onChange={trackPassword} value={password} placeholder="password"></input>
+                <input type="password" onChange={trackPassword} value={password} placeholder="password"
+                ></input>
                 <br></br>
-                <button type='click' onClick={printDetails}>Login</button>
-              </>
-
+                <button type="click" onClick={printDetails}
+                >Login</button></>
       }
     </>
   )
 }
-
 export default Login
